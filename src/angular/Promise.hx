96361a32a9b40f6b8c21c41285f81ebac7ae3ab6
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2014-12-08 18:18:51 +0100

    add thenFlat Promise

--- src/angular/Promise.hx
+++ src/angular/Promise.hx
@@ -2,5 +2,8 @@
 package angular;
 
 extern class Promise<T> {
+	@:native("then")
+	public function thenFlat<U>(f : T -> Promise<U>) : Promise<U>;
+
 	public function then<U>(f : T -> U) : Promise<U>;
 }
