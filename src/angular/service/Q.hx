4f114829fae968d3467bcf6a66ab18f6e9b4b782
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2014-12-10 12:31:37 +0100

    add Q signature

--- src/angular/service/Q.hx
+++ src/angular/service/Q.hx
@@ -1,9 +1,23 @@
 
 package angular.service;
 
+extern class Deferred<T> {
+	public var promise : Promise<T>;
+}
+
 @:injectionName("$q")
 extern class Q {
 
 	public function all<T>(arr : Array<Promise<T> >) : Promise<Array<T> >;
+
+	@:selfCall
+	public function create<T, U>(resolve : T -> Void, reject : U -> Void) : Promise<T>;
+
+	public function reject<T, U>(reason : U) : Promise<T>;
+
+	public function defer<T>() : Deferred<T>;
+
+	@:overload(function<T>(v : T) : Promise<T> {})
+	public function when<T>(promise : Promise<T>) : Promise<T>;
 
 }
