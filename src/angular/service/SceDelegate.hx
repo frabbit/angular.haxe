74fd56fa51d922581acd4f7d2c14f446e5a4a9ec
Adrien Jonquet <adrien.jonquet@gmail.com>
2014-12-11 23:33:43 +0100

    add trustAs(type : String, value : Dynamic) : Dynamic in SceDelegate;

--- src/angular/service/SceDelegate.hx
+++ src/angular/service/SceDelegate.hx
@@ -2,4 +2,6 @@
 package angular.service;
 
 @:injectionName("$sceDelegate")
-extern class SceDelegate {}
+extern class SceDelegate {
+	public function trustAs(type : String, value : Dynamic) : Dynamic;
+}
