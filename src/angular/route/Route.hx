9a622566d373f298fde0078cf44b28f82ae46bcb
Ivan Tivonenko <darkdarkdragon@gmail.com>
2015-03-03 20:33:03 +0200

    improve $routeProvider and $route externs

--- src/angular/route/Route.hx
+++ src/angular/route/Route.hx
@@ -4,4 +4,35 @@ package angular.route;
 
 @:injectionName("$route")
 extern class Route {
+
+    /**
+     * Reference to the current route definition. The route definition contains:
+     *   controller: The controller constructor as define in route definition.
+     *   locals: A map of locals which is used by $controller service for controller
+     *           instantiation. The locals contain the resolved values of the resolve
+     *           map. Additionally the locals also contain:
+     *              $scope - The current route scope.
+     *              $template - The current route template HTML.
+     */
+    public var current: Dynamic;
+
+    /**
+     * Object with all route configuration Objects as its properties.
+     */
+    public var current: Dynamic;
+
+    /**
+     * Causes $route service to reload the current route even if $location hasn't changed.
+     * As a result of that, ngView creates new scope and reinstantiates the controller.
+     */
+    public function reload();
+
+    /**
+     * Causes $route service to update the current URL, replacing current route parameters
+     * with those specified in newParams. Provided property names that match the route's
+     * path segment definitions will be interpolated into the location's path, while
+     * remaining properties will be treated as query params.
+     * @param newParams Dynamic
+     */
+    public function updateParams(newParams: Dynamic);
 }
--- src/angular/route/RouteProvider.hx
+++ src/angular/route/RouteProvider.hx
@@ -3,4 +3,42 @@ package angular.route;
 
 @:injectionName("$routeProvider")
 extern class RouteProvider {
+
+    /**
+     * Adds a new route definition to the $route service.
+     *
+     * @param path
+     * @param route
+     * @return
+     */
+	public function when(path : String, route : RouteProviderRoute) : RouteProvider;
+
+	@:overload(function(path : String) : RouteProvider {})
+    public function otherwise(route : RouteProviderRoute) : RouteProvider;
+}
+
+
+typedef RouteProviderRoute = {
+	// controller – {(string|function()=} – Controller fn that should be associated with newly created
+    // scope or the name of a registered controller if passed as a string.
+    ?controller : Dynamic,
+    // controllerAs – {string=} – An identifier name for a reference to the controller. If present, the controller
+    // will be published to scope under the controllerAs name.
+    ?controllerAs : String,
+    // {string=|function()=}  – html template as a string or a function that returns an
+    // html template as a string which should be used by ngView or ngInclude directives.
+    // This property takes precedence over templateUrl.
+    ?template : Dynamic,
+    // templateUrl – {string=|function()=} – path or function that returns a path to an html template that should be used by ngView.
+    ?templateUrl : Dynamic,
+    // resolve - {Object.<string, function>=} - An optional map of dependencies which should be injected into the controller.
+    ?resolve : Dynamic,
+    // redirectTo – {(string|function())=} – value to update $location path with and trigger route redirection.
+    ?redirectTo : Dynamic,
+    // [reloadOnSearch=true] - {boolean=} - reload route when only $location.search() or $location.hash() changes.
+    // If the option is set to false and url in the browser changes, then $routeUpdate event is broadcasted on the root scope.
+    ?reloadOnSearch : Bool,
+    // caseInsensitiveMatch=false] - {boolean=} - match routes without being case sensitive
+    // If the option is set to true, then the particular route can be matched without being case sensitive
+    ?caseInsensitiveMatch : Bool
 }
