d7ab144a3a46b4c7e2a1c34c98c1953bd634668c
frabbit <frabbit77@googlemail.com>
2015-03-08 02:42:34 +0100

    add alternative to sample 04 with multiple typedefs

---
+++ sample/sample-05/src/Main.hx
@@ -0,0 +1,35 @@
+package ;
+
+import angular.*;
+import angular.service.*;
+
+
+
+
+typedef CustomString<Const> = String;
+
+typedef Url = String;
+typedef Title = String;
+typedef Description = String;
+
+typedef AppScope = {
+	data : { url : String, title : String, description : String }
+};
+
+class Main
+{
+	static function main ()
+	{
+		var module =
+			Angular.module("myModule", [])
+			.factory( function ():Url return "https://github.com/frabbit/angular.haxe")
+			.factory( function ():Title return "angular.haxe")
+			.factory( function ():Description return "Some words about angular.haxe...")
+
+
+			.controller("AppController", function (s:TypedScope<AppScope>, url:Url, title : Title, desc:Description) {
+
+				s.data = { url : url, title : title, description : desc };
+			});
+	}
+}
